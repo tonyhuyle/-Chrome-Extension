5a72400dc6da935fa2d905d2f9f1b4522f20c519
Your Name <tonyhuyle123@gmail.com>
2024-10-26 13:16:39 -0500

    Update js

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
